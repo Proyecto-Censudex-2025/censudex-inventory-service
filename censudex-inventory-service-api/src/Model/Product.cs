8c490ee746c914c01007197effba193197305cd2
IgnacioC <Ignacio.Carvajal@alumnos.ucn.cl>
2025-10-27 18:36:48 -0300

    feat: initial structure added

---
+++ censudex-inventory-service-api/src/Model/Product.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace censudex_inventory_service_api.src.Model
+{
+    public class Product
+    {
+        
+    }
+}
